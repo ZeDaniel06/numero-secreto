7844c8bd24390277059ee75a85c0377700daecce
ZeDaniel06 <silvadan752@gmail.com>
2025-07-29 13:45:09 -0300

    Limite de números aumentado para 100

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function gerarNumeroAleatorio(){
 
 function mensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function novoJogo(){
@@ -66,7 +66,7 @@ function novoJogo(){
 }
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
